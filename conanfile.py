f09ee48309170a2d4c62b55f0c4fa3675c75b218
SSE4 <tomskside@gmail.com>
2018-10-04 20:49:08 +0700

    - update to 2018R2, use an official installer
    Signed-off-by: SSE4 <tomskside@gmail.com>

--- conanfile.py
+++ conanfile.py
@@ -8,35 +8,48 @@ import os
 
 class IntelMediaSDKConan(ConanFile):
     name = "intel_media_sdk"
-    version = "2017R1"
+    version = "2018R2"
     url = "https://github.com/bincrafters/conan-intel_media_sdk"
     description = "Intel® Media SDK provides an API to access hardware-accelerated video decode, encode and " \
                   "filtering on Intel® platforms with integrated graphics."
     license = "MIT"
     exports = ["LICENSE.md"]
     settings = {"os": ["Windows"], "arch": ["x86", "x86_64"], "compiler": ["Visual Studio"]}
-    source_subfolder = 'intel_media_sdk'
 
     def source(self):
-        source_url = "https://github.com/SSE4/intel_media_sdk.git"
-        self.run('git clone %s --depth 1 --branch %s' % (source_url, self.version))
+        source_url = "http://registrationcenter-download.intel.com/akdlm/irc_nas/vcp/13618/MSDK2018R2.exe"
+        tools.download(source_url, 'MSDK2018R2.exe')
+
+    def build(self):
+        for action in ['remove', 'install']:
+            self.run('MSDK2018R2.exe '
+                     '%s '
+                     '--silent '
+                     '--installdir=%s '
+                     '--eval '
+                     '--eula=accept '
+                     '--output=out.txt '
+                     '--send-data=no '
+                     '--update=always' % (action, os.getcwd()))
 
     def package(self):
-        self.copy(pattern="Media_SDK_EULA.rtf", dst="licenses", src=self.source_subfolder)
-        self.copy(pattern="mediasdk_release_notes.pdf", dst="licenses", src=self.source_subfolder)
-        self.copy(pattern="redist.txt", dst="licenses", src=self.source_subfolder)
-        self.copy(pattern="redist.txt", dst="licenses", src=self.source_subfolder)
+        install_dir = os.path.join('Intel(R) Media SDK 2018 R2', 'Software Development Kit')
 
-        self.copy(pattern="*.h", dst=os.path.join('include', 'mfx'), src=os.path.join(self.source_subfolder, 'include'))
+        self.copy(pattern="Media_SDK_EULA.rtf", dst="licenses", src=install_dir)
+        self.copy(pattern="mediasdk_release_notes.pdf", dst="licenses", src=install_dir)
+        self.copy(pattern="redist.txt", dst="licenses", src=install_dir)
+        self.copy(pattern="redist.txt", dst="licenses", src=install_dir)
+
+        self.copy(pattern="*.h", dst=os.path.join('include', 'mfx'), src=os.path.join(install_dir, 'include'))
         if self.settings.arch == 'x86':
-            self.copy(pattern="*.lib", dst="lib", src=os.path.join(self.source_subfolder, 'lib', 'Win32'),
+            self.copy(pattern="*.lib", dst="lib", src=os.path.join(install_dir, 'lib', 'Win32'),
                       keep_path=True)
-            self.copy(pattern="*.dll", dst="bin", src=os.path.join(self.source_subfolder, 'bin', 'Win32'),
+            self.copy(pattern="*.dll", dst="bin", src=os.path.join(install_dir, 'bin', 'Win32'),
                       keep_path=True)
         elif self.settings.arch == 'x86_64':
-            self.copy(pattern="*.dll", dst="bin", src=os.path.join(self.source_subfolder, 'bin', 'Win32'),
+            self.copy(pattern="*.dll", dst="bin", src=os.path.join(install_dir, 'bin', 'Win32'),
                       keep_path=True)
-            self.copy(pattern="*.lib", dst="lib", src=os.path.join(self.source_subfolder, 'lib', 'x64'),
+            self.copy(pattern="*.lib", dst="lib", src=os.path.join(install_dir, 'lib', 'x64'),
                       keep_path=True)
 
         with tools.chdir(os.path.join(self.package_folder, 'lib')):
